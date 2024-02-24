5a4b4e2b6980b0f91c83d60d6867e04745df6958
aqwarius2003 <144896702+aqwarius2003@users.noreply.github.com>
2024-02-24 12:53:27 +0400

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import requests
 import os
 from urllib.parse import urlparse
-BITLINK_API = 'https://api-ssl.bitly.com/v4/bitlinks'
+BITLY_API = 'https://api-ssl.bitly.com/v4/bitlinks'
 
 
 def is_bitlink(token, url):
     headers = {'Authorization': f'Bearer {token}'}
     parsed_url = urlparse(url)
     bitlink_url = parsed_url.netloc + parsed_url.path
-    response = requests.get(f'{BITLINK_API}/{bitlink_url}', headers=headers)
+    response = requests.get(f'{BITLY_API}/{bitlink_url}', headers=headers)
     if response.ok:
         return True
 
@@ -18,7 +18,7 @@ def get_shorten_link(token, url):
     if response.ok:
         headers = {'Authorization': f'Bearer {token}'}
         data_url = {'long_url': url}
-        response = requests.post(BITLINK_API, headers=headers, json=data_url)
+        response = requests.post(BITLY_API, headers=headers, json=data_url)
         if response.ok:
             bitlink = response.json().get('link')
             return bitlink
@@ -27,7 +27,7 @@ def get_shorten_link(token, url):
 def get_count_cliks(token, url):
     headers = {'Authorization': f'Bearer {token}'}
     bitlink = url.replace('https://', '').replace('http://', '')
-    clicks_bitlink_link = f'{BITLINK_API}/{bitlink}/clicks/summary'
+    clicks_bitlink_link = f'{BITLY_API}/{bitlink}/clicks/summary'
     response = requests.get(clicks_bitlink_link, headers=headers)
     response.raise_for_status()
     click_count = response.json()['total_clicks']
@@ -35,7 +35,7 @@ def get_count_cliks(token, url):
 
 
 def main():
-    token = os.environ['BITLINK_TOKEN']
+    token = os.environ['BITLY_TOKEN']
     url = input('Введите ссылку: ')
     response = requests.get(url)
     if not response.ok:
@@ -44,14 +44,14 @@ def main():
     if is_bitlink(token, url):
         try:
             click_count = get_count_cliks(token, url)
-            print(f'По ссылке {url} было совершено {click_count} переходов')
+            print(f'По ссылке {url}\nбыло совершено {click_count} кликов')
         except requests.exceptions.HTTPError as error:
             print('При проверке количества кликов ошибка:\n{0}'.format(error))
             return
     else:
         try:
             bitlink = get_shorten_link(token, url)
-            print(f'Для адреса {url} \nкороткая ссылка: {bitlink}')
+            print(f'Короткая ссылка: {bitlink}')
         except requests.exceptions.HTTPError as error:
             print('При пролучении короткой ссылки ошибка:\n{0}'.format(error))
 
