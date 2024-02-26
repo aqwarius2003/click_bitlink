ffa0ec2663501c3649f8275febd5ee9e71661c9e
aqwarius2003 <144896702+aqwarius2003@users.noreply.github.com>
2024-02-27 01:30:17 +0400

    main.py

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
 import requests
 import os
 from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+
 BITLY_API = 'https://api-ssl.bitly.com/v4/bitlinks'
 
 
 def is_bitlink(token, url):
     headers = {'Authorization': f'Bearer {token}'}
     parsed_url = urlparse(url)
-    bitlink_url = parsed_url.netloc + parsed_url.path
+    bitlink_url = f'{parsed_url.netloc}{parsed_url.path}'
     response = requests.get(f'{BITLY_API}/{bitlink_url}', headers=headers)
-    if response.ok:
-        return True
+    return response.ok
 
 
-def get_shorten_link(token, url):
-    response = requests.get(url)
-    if response.ok:
-        headers = {'Authorization': f'Bearer {token}'}
-        deeplinks = {'long_url': url}
-        response = requests.post(BITLY_API, headers=headers, json=deeplinks)
-        if response.ok:
-            bitlink = response.json().get('link')
-            return bitlink
+def shorten_link(token, url):
+    headers = {'Authorization': f'Bearer {token}'}
+    long_url = {'long_url': url}
+    response = requests.post(BITLY_API, headers=headers, json=long_url)
+    bitlink = response.json().get('link')
+    return bitlink
 
 
-def get_count_cliks(token, url):
+def count_cliks(token, url):
     headers = {'Authorization': f'Bearer {token}'}
     parsed_url = urlparse(url)
-    bitlink_url = parsed_url.netloc + parsed_url.path
+    bitlink_url = f'{parsed_url.netloc}{parsed_url.path}'
     clicks_bitlink_link = f'{BITLY_API}/{bitlink_url}/clicks/summary'
     response = requests.get(clicks_bitlink_link, headers=headers)
     response.raise_for_status()
@@ -36,25 +35,18 @@ def get_count_cliks(token, url):
 
 
 def main():
-    token = os.environ['BITLY_TOKEN']
+    load_dotenv()
+    token = os.getenv('BITLY_TOKEN')
     url = input('Введите ссылку: ')
-    response = requests.get(url)
-    if not response.ok:
-        print('URL-адрес недоступен')
-        return
-    if is_bitlink(token, url):
-        try:
-            click_count = get_count_cliks(token, url)
+    try:
+        if is_bitlink(token, url):
+            click_count = count_cliks(token, url)
             print(f'По ссылке {url}\nбыло совершено {click_count} кликов')
-        except requests.exceptions.HTTPError as error:
-            print('При проверке количества кликов ошибка:\n{0}'.format(error))
-            return
-    else:
-        try:
-            bitlink = get_shorten_link(token, url)
+        else:
+            bitlink = shorten_link(token, url)
             print(f'Короткая ссылка: {bitlink}')
-        except requests.exceptions.HTTPError as error:
-            print('При пролучении короткой ссылки ошибка:\n{0}'.format(error))
+    except requests.exceptions.HTTPError as error:
+        print('Произошла ошибка:\n{0}'.format(error))
 
 
 if __name__ == "__main__":
